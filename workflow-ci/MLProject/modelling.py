3ad5781cdd9724d1d9ec524c2bf06ede53f98717
hooddz1 <huuddz1@gmail.com>
2025-12-24 03:55:15 +0700

    fix modelling & yml script

--- workflow-ci/MLProject/modelling.py
+++ workflow-ci/MLProject/modelling.py
@@ -15,7 +15,7 @@ parser.add_argument("--n_estimators", type=int, default=100)
 parser.add_argument("--max_depth", type=int, default=10)
 args = parser.parse_args()
 
-# 2. SETUP autorisasi DagsHub
+# 2. SETUP authoriasi DagsHub
 try:
     user = os.environ.get('DAGSHUB_USERNAME', 'hooddz1')
     token = os.environ.get('DAGSHUB_TOKEN', '')
